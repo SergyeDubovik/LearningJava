f586acf93ff5c96c411113b46bfc3253369ea755
Serg <supermirko9@gmail.com>
2024-11-22 23:59:20 +0200

    improve coin task

--- src/com/coinFlip/Coin.java
+++ src/com/coinFlip/Coin.java
@@ -6,20 +6,23 @@ public class Coin {
     public static void main(String[] args) {
         int heads = 0;
         int tails = 0;
-        int throwCount = 10;
+        int throwsCount = 10;
         Random random = new Random();
-        for (int i = 1; i <= throwCount; i++) {
+        for (int i = 1; i <= throwsCount; i++) {
             boolean side = random.nextBoolean();
             if (side) {
                 heads++;
             } else {
                 tails++;
             }
+            System.out.print("heads: " + heads);
+            System.out.print(", tails: " + tails + '\n');
         }
-        double percentHeads = ((double) heads / throwCount) * 100;
-        double percentTails = ((double) tails / throwCount) * 100;
-        System.out.println(percentHeads);
-        System.out.println(percentTails);
+        System.out.println();
+        double percentHeads = ((double) heads / throwsCount) * 100;
+        double percentTails = ((double) tails / throwsCount) * 100;
+        System.out.println("Heads: " + percentHeads + "%");
+        System.out.println("Tails: " + percentTails + "%");
 
     }
 }
