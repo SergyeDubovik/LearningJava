e58525487edf27f7befffba0e6e55b984e9218d7
Serg <supermirko9@gmail.com>
2024-11-23 21:48:11 +0200

    made coin task

--- src/com/coinFlip/Coin.java
+++ src/com/coinFlip/Coin.java
@@ -5,34 +5,38 @@ import java.util.Random;
 public class Coin {
     private int heads;
     private int tails;
-    private final int checkPeriod;
     private final int throwsCount;
     private final Random random;
 
-    public Coin(int throwsCount, int checkPeriod) {
+    public Coin(int throwsCount) {
         this.heads = 0;
         this.tails = 0;
-        this.checkPeriod = checkPeriod;
         this.random = new Random();
         this.throwsCount = throwsCount;
     }
-    public void throwing() {
+
+    public void throwCoin() {
+        int step = 10;
         for (int i = 1; i <= throwsCount; i++) {
             if (random.nextBoolean()) {
                 heads++;
             } else {
                 tails++;
             }
-            if (i % checkPeriod == 0) {
+            if (i == step) {
+                double headsPercent = ((double) heads / i) * 100;
+                double tailsPercent = ((double) tails / i) * 100;
+                step *= 10;
                 System.out.print("iteration " + i + " - ");
-                System.out.print("heads: " + heads + ", tails: " + tails);
+                System.out.print("heads: " + heads + ", tails: " + tails +
+                        ",  percent: Heads - " + headsPercent + " Tails -  " + tailsPercent);
                 System.out.println();
             }
-
         }
         outcome();
     }
-    public void outcome () {
+
+    private void outcome() {
         double percentHeads = ((double) heads / throwsCount) * 100;
         double percentTails = ((double) tails / throwsCount) * 100;
         System.out.println("Heads: " + percentHeads + "%");
--- src/com/coinFlip/CoinDemo.java
+++ src/com/coinFlip/CoinDemo.java
@@ -2,9 +2,7 @@ package com.coinFlip;
 
 public class CoinDemo {
     public static void main(String[] args) {
-        Coin coin = new Coin(100, 10);
-        coin.throwing();
-        Coin anotherOne = new Coin(1000, 100);
-        anotherOne.throwing();
+        Coin coin = new Coin(10000000);
+        coin.throwCoin();
     }
 }
