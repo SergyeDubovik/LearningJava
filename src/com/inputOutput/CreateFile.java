9cebee014454b862a92153d60a68a497146d9e74
Serg <supermirko9@gmail.com>
2025-01-13 14:22:13 +0200

    read some text from the file to console

--- src/com/inputOutput/CreateFile.java
+++ src/com/inputOutput/CreateFile.java
@@ -1,19 +1,37 @@
 package com.inputOutput;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String text = scan.nextLine();
+        String text = "pizza better than big mac";
+
+        writeToFile(text);
+        readFromFile();
+
+    }
+//        написать программу, которая умеет записывать в файл, то что я ввел с клавиатуры
+
+    private static void writeToFile(String text) throws IOException {
         File file = new File("newFileHere/note.txt");
         System.out.println(file.getAbsolutePath());
         try (OutputStream os = new FileOutputStream(file)) {
             os.write(text.getBytes());
         }
     }
+
+//        написать программу, которая умеет читать с файла и печатать на экран
+
+
+    private static void readFromFile() throws IOException {
+        File newFile = new File("newFileHere/note.txt");
+        System.out.println(newFile.getAbsolutePath());
+        try (InputStream inStr = new FileInputStream(newFile)) {
+            LineNumberReader nr = new LineNumberReader(new InputStreamReader(inStr));
+            String s = nr.readLine();
+            System.out.println(s);
+        }
+    }
+
 }
