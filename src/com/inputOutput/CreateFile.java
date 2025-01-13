271555fefec31dc9f172ed61b7a6deff79500287
Serg <supermirko9@gmail.com>
2025-01-14 00:01:28 +0200

    sorted input array

--- src/com/inputOutput/CreateFile.java
+++ src/com/inputOutput/CreateFile.java
@@ -3,6 +3,8 @@ package com.inputOutput;
 import java.io.*;
 import java.util.Scanner;
 
+import static testArrays.TaskArrays.printArray;
+
 public class CreateFile {
     public static void main(String[] args) throws IOException {
         String text = "pizza better than big mac";
@@ -12,6 +14,7 @@ public class CreateFile {
 
         writeToFile(text, file);
         readFromFile(file);
+        sortInputArray(5);
 
     }
 //        написать программу, которая умеет записывать в файл, то что я ввел с клавиатуры
@@ -24,7 +27,6 @@ public class CreateFile {
 
 //        написать программу, которая умеет читать с файла и печатать на экран
 
-
     private static void readFromFile(File f) throws IOException {
         try (InputStream inStr = new FileInputStream(f)) {
             LineNumberReader nr = new LineNumberReader(new InputStreamReader(inStr));
@@ -33,4 +35,25 @@ public class CreateFile {
         }
     }
 
+//    написать программу, котоая читает массив чисел с клавы, сортирует их и печатает
+
+    private static void sortInputArray(int size) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbs = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbs[i] = sc.nextInt();
+        }
+        System.out.println("Sorted array: ");
+        int tmp;
+        for (int j = 1; j < size; j++) {
+            for (int u = j; u > 0; u--) {
+                if(numbs[u] > numbs[u - 1]) {
+                    tmp = numbs[u];
+                    numbs[u] = numbs[u - 1];
+                    numbs[u - 1] = tmp;
+                }
+            }
+        }
+        printArray(numbs);
+    }
 }
