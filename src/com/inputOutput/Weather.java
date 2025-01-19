c776fe3704368dd1f9ee99076b2581f97d2a5891
Serg <supermirko9@gmail.com>
2025-01-19 20:02:29 +0200

    added task - calculating average weather per year

--- src/com/inputOutput/Weather.java
+++ src/com/inputOutput/Weather.java
@@ -1,6 +1,8 @@
 package com.inputOutput;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Weather {
 
@@ -13,26 +15,59 @@ public class Weather {
 //              1.5 берем первые (24*365) элементов, считаем по ним среднее и выводим на экран
 //              1.6 повторяем для следующих 24*365 элементов
 //              1.7 бонус - высчитать температуру харькова также
+    private static final int MEASUREMENTS_PER_YEAR = 24 * 365;
 
     public static void main(String[] args) throws IOException {
-        File file = new File("C:/Users/Маша/Downloads/open-meteo.csv");
-        readFile(file);
-
+        File file = new File("C:/Users/Сергей/Downloads/open-meteo-40.74N73.91W9m.csv");
+        calculateAverageByYear(file);
     }
 
-    private static void readFile(File file) throws IOException {
+    private static List<Double> readFile(File file) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(file));
         String text;
         for (int i = 0; i < 4; i++) {
             br.readLine();
         }
         String temperature;
+        double result;
+        List<Double> degreesCelsius = new ArrayList<>();
         while ((text = br.readLine()) != null) {
-            int delimeter = text.indexOf(',');
-            temperature = text.substring(delimeter + 1);
-            System.out.println(temperature);
+            int delimiter = text.indexOf(',');
+            temperature = text.substring(delimiter + 1);
+            result = Double.parseDouble(temperature);
+            degreesCelsius.add(result);
         }
+        return degreesCelsius;
+    }
 
+    private static void calculateAverageByYear(File file) throws IOException {
+        List<Double> temperatures = readFile(file);
+        List<List<Double>> temperaturesByYear = split(temperatures);
+        for (List<Double> numbers : temperaturesByYear) {
+            System.out.println(average(numbers));
+        }
+    }
+
+    private static List<List<Double>> split(List<Double> degrees) {
+        int wholeCount = degrees.size();
+        int sumYears = wholeCount / MEASUREMENTS_PER_YEAR;
+        List<List<Double>> temperatureByYear = new ArrayList<>();
+        for (int i = 0; i < sumYears; i++) {
+            List<Double> oneYear = new ArrayList<>(degrees.subList(MEASUREMENTS_PER_YEAR * i,
+                    MEASUREMENTS_PER_YEAR * (i + 1)));
+            temperatureByYear.add(oneYear);
+        }
+        return temperatureByYear;
+    }
+
+
+    // calculating average from specified piece of list
+    private static double average(List<Double> numbers) {
+        double sum = 0;
+        for (double n : numbers) {
+            sum += n;
+        }
+        return sum / numbers.size();
     }
 
 }
