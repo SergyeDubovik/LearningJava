9a5c57c537c9f86447f88e94e376809f1b98f7a3
Serg <supermirko9@gmail.com>
2025-02-12 23:59:28 +0200

    improved menu in library

--- src/librarySystem/LibraryCLI.java
+++ src/librarySystem/LibraryCLI.java
@@ -1,13 +1,7 @@
 package librarySystem;
 
-import com.library.Book;
 import com.library.PrintedProduction;
-import com.library.filter.FromYearTillYearFilter;
-import com.library.filter.GenreFilter;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 import java.util.Scanner;
 
 import static librarySystem.RunLibrary.buildLibrary;
@@ -17,42 +11,23 @@ public class LibraryCLI {
 
         Library library = buildLibrary();
 
-        Scanner in = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to library.");
+        runMainMenu(userInput, library);
+    }
+
+    private static void runMainMenu(Scanner userInput, Library library) {
         boolean exit = false;
         while (!exit) {
-            System.out.println();
-            System.out.println("What would you like to do?");
-            System.out.println("1 - Show available books");
-            System.out.println("2 - Get book");
-            System.out.println("3 - Return book");
-            System.out.println("0 - Exit");
-            System.out.println("Your choice:");
+            mainMenuDisplay();
 
-            String input = in.nextLine();
+            String input = userInput.nextLine();
 
             switch (input) {
                 case "1":
-                    System.out.println("Available books:");
-                    System.out.println("    1 - Show all books");
-                    System.out.println("    2 - Filter by Author");
-                    System.out.println("    3 - Filter by From year till year");
-                    System.out.println("    4 - Filter by Genre");
-                    System.out.println("    5 - Filter by Released after ");
-                    System.out.println("    0 - Return to previous menu ");
-                    String subMenuInput = in.nextLine();
-                    switch (subMenuInput) {
-                        case "1":
-                            for (PrintedProduction production : library.getBooks()) {
-                                System.out.println(production);
-                            }
-                            break;
-                        case "0":
-                            break;
-                        default:
-                            System.out.println("Please input correct value");
-                            break;
-                    }
+                    subMenuDisplay();
+
+                        runSubMenu(userInput, library);
                     break;
                 case "2":
                     System.out.println("Implementation of getting book will be added soon");
@@ -72,5 +47,47 @@ public class LibraryCLI {
                     break;
             }
         }
+    }
+
+    private static void runSubMenu(Scanner in, Library library) {
+        boolean backToMainMenu = false;
+        while (!backToMainMenu) {
+        String subMenuInput = in.nextLine();
+        switch (subMenuInput) {
+            case "1":
+                for (PrintedProduction production : library.getBooks()) {
+                    System.out.println(production);
+                }
+                subMenuDisplay();
+                break;
+            case "0":
+                System.out.println("Get back to main menu");
+                backToMainMenu = true;
+                break;
+            default:
+                System.out.println("Please input correct value");
+                break;
+        }
+    }
+    }
+
+    private static void subMenuDisplay() {
+        System.out.println("Available books:");
+        System.out.println("    1 - Show all books");
+        System.out.println("    2 - Filter by Author");
+        System.out.println("    3 - Filter by From year till year");
+        System.out.println("    4 - Filter by Genre");
+        System.out.println("    5 - Filter by Released after ");
+        System.out.println("    0 - Return to previous menu ");
+    }
+
+    private static void mainMenuDisplay() {
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println("1 - Show available books");
+        System.out.println("2 - Get book");
+        System.out.println("3 - Return book");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice:");
     }
 }
