2f2661a889b1d55a9d617d505bdfbe338a11d13d
Serg <supermirko9@gmail.com>
2025-02-16 22:51:34 +0200

    create DirJ and added files with their size by space divider

---
+++ src/com/dirj/DirJ.java
@@ -0,0 +1,32 @@
+package com.dirj;
+
+import java.io.File;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class DirJ {
+    public static void main(String[] args) {
+        String currentDirName = System.getProperty("user.dir");
+        File currentDir = new File(currentDirName);
+        printDir(currentDir);
+    }
+
+    private static void printDir(File currentDir) {
+        File[] children = currentDir.listFiles();
+        NumberFormat nf = NumberFormat.getInstance(Locale.FRANCE);
+        if (children == null) {
+            throw new RuntimeException();
+        }
+        System.out.println("          .");
+        System.out.println("          ..");
+        for (File child : children) {
+            if (child.isFile()) {
+                String formatted = nf.format(child.length());
+                System.out.printf("%8s", formatted);
+            } else {
+                System.out.print("        ");
+            }
+            System.out.println("  " + child.getName());
+        }
+    }
+}
