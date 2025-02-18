6cbf92ccdc526417ba827b26c615653381d0d222
Serg <supermirko9@gmail.com>
2025-02-18 23:57:14 +0200

    added date and time to new left column

--- src/com/dirj/DirJ.java
+++ src/com/dirj/DirJ.java
@@ -2,12 +2,15 @@ package com.dirj;
 
 import java.io.File;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class DirJ {
     public static void main(String[] args) {
         String currentDirName = System.getProperty("user.dir");
         File currentDir = new File(currentDirName);
+
         printDir(currentDir);
     }
 
@@ -17,16 +20,28 @@ public class DirJ {
         if (children == null) {
             throw new RuntimeException();
         }
-        System.out.println("<DIR>" + " ".repeat(16) + '.');
-        System.out.println("<DIR>" + " ".repeat(16) + "..");
+        date(currentDir);
+        System.out.println("  <DIR>" + " ".repeat(16) + '.');
+        date(currentDir);
+        System.out.println("  <DIR>" + " ".repeat(16) + "..");
         for (File child : children) {
+            date(child);
             if (child.isFile()) {
                 String formatted = nf.format(child.length());
-                System.out.printf("%19s", formatted);
+                System.out.printf("%21s", formatted);
             } else {
-                System.out.print("<DIR>" + " ".repeat(14));
+                System.out.print("  <DIR>" + " ".repeat(14));
             }
             System.out.println("  " + child.getName());
         }
     }
+
+    private static void date(File child) {
+        long dateTime = child.lastModified();
+        Date date = new Date(dateTime);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String formatDate = sdf.format(date);
+        System.out.print(formatDate);
+    }
+
 }
