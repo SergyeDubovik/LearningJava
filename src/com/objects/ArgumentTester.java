84002c89949016331d56b7f802ca0a558c1bf0ab
Serg <supermirko9@gmail.com>
2025-03-08 15:41:16 +0200

    added class ArgumentTester

---
+++ src/com/objects/ArgumentTester.java
@@ -0,0 +1,9 @@
+package com.objects;
+
+public class ArgumentTester {
+    public static void main(String[] args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+}
