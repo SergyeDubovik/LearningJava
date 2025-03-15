73144f044a5e25081772872f253d8d1fcd8d8b02
Serg <supermirko9@gmail.com>
2025-03-15 15:09:04 +0200

    added equals and hashCode to class Customer

--- src/librarySystem/Customer.java
+++ src/librarySystem/Customer.java
@@ -1,5 +1,7 @@
 package librarySystem;
 
+import java.util.Objects;
+
 public class Customer {
 
     private final String name;
@@ -11,5 +13,18 @@ public class Customer {
     @Override
     public String toString() {
         return "Customer: " + name + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
