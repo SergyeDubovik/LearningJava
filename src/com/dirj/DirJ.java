cf86578244a6984b27dfe7b5262e74a56b7ee96b
Serg <supermirko9@gmail.com>
2025-03-19 21:19:50 +0200

    fixed code review comments

--- src/com/dirj/DirJ.java
+++ src/com/dirj/DirJ.java
@@ -1,31 +1,26 @@
 package com.dirj;
 
 import java.io.File;
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class DirJ {
-    private static final String DIR = "  <DIR>";
-    private static boolean onlyFiles = false;
+    public static final String DIR = "  <DIR>";
+    private static boolean onlyFileNames = false;
     private static boolean lowerCase = false;
 
     public static void main(String[] args) {
         for (String arg : args) {
-            boolean helpOption = false;
             switch (arg.toUpperCase()) {
                 case "/B":
-                    onlyFiles = true;
+                    onlyFileNames = true;
                     break;
                 case "/L":
                     lowerCase = true;
                     break;
                 case "/?":
-                    if (!helpOption) {
-                        helpMessage();
-                        return;
-                    }
-                    break;
+                    helpMessage();
+                    return;
                 default:
                     System.out.println("Unknown argument '" + arg + "'. Use '/?' argument for help.");
                     return;
@@ -39,46 +34,35 @@ public class DirJ {
 
     private static void printDir(File currentDir) {
         File[] children = currentDir.listFiles();
-        NumberFormat nf = NumberFormat.getInstance(Locale.FRANCE);
-        StringBuilder sb = new StringBuilder();
         if (children == null) {
             throw new RuntimeException();
         }
-        if (!onlyFiles) {
+        Formatter formatter;
+        if (onlyFileNames) {
+            formatter = new FileNameFormatter(lowerCase);
+        } else {
+            formatter = new FullInfoFormatter(lowerCase);
+        }
+        StringBuilder sb = new StringBuilder();
+        if (!onlyFileNames) {
             sb.append(getLastModifiedDate(currentDir));
             sb.append(DIR).append(" ".repeat(16)).append('.').append('\n');
             sb.append(getLastModifiedDate(currentDir.getParentFile()));
             sb.append(DIR).append(" ".repeat(16)).append("..").append('\n');
         }
-        for (File child : children) {
-            if (!onlyFiles) {
-                sb.append(getLastModifiedDate(child));
-            }
-            if (child.isFile()) {
-                String formatted = nf.format(child.length());
-                if (!onlyFiles) {
-                    sb.append(String.format("%21s", formatted));
-                }
-            } else {
-                if (!onlyFiles) {
-                    sb.append(DIR).append(" ".repeat(14));
-                }
-            }
-            if (lowerCase) {
-                sb.append("  ").append(child.getName().toLowerCase()).append('\n');
-            } else {
-                sb.append("  ").append(child.getName()).append('\n');
-            }
-        }
         System.out.print(sb);
+        for (File child : children) {
+            System.out.println(formatter.format(child));
+        }
     }
 
-    private static String getLastModifiedDate(File child) {
+    public static String getLastModifiedDate(File child) {
         long dateTime = child.lastModified();
         Date date = new Date(dateTime);
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
         return sdf.format(date) + " ";
     }
+
     private static void helpMessage() {
         String message = """
                 /B - Show only files
---
+++ src/com/dirj/Formatter.java
@@ -0,0 +1,7 @@
+package com.dirj;
+
+import java.io.File;
+
+public interface Formatter {
+    String format(File file);
+}
