8d7efd88f7b26d5b1f38505c7d767a5effafa4e2
Serg <supermirko9@gmail.com>
2025-03-21 23:36:42 +0200

    worked with ArgumentTester with new features

--- src/com/objects/ArgumentTester.java
+++ src/com/objects/ArgumentTester.java
@@ -2,8 +2,14 @@ package com.objects;
 
 public class ArgumentTester {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         for (String s : args) {
-            System.out.println(s);
+            sb.append(s);
+            if (s.equals("hello")) {
+                System.out.println(s.toUpperCase());
+            } else {
+                System.out.println(sb);
+            }
         }
     }
 }
