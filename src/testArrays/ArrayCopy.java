ae59499d7a6f995ee8270eac74143fc98b32906a
Serg <supermirko9@gmail.com>
2025-03-26 21:36:51 +0200

    refactored method arraycopy

--- src/testArrays/ArrayCopy.java
+++ src/testArrays/ArrayCopy.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 
 public class ArrayCopy {
     public static void main(String[] args) {
-        int[] first = {1, 5, 23, 11};
+        int[] first = {1};
         int[] second = {3, 12, 7, 8, 10, -4};
-        arraycopy(first, 1, second, 2, 2);
+        int[] array = new int[0];
+        int[] array2 = new int[0];
+        arraycopy(first, 0, second, 0, 1);
         System.out.println(Arrays.toString(second));
     }
 
-    private static void arraycopy(int[] arr1, int sourcePos, int[] arr2, int destPos, int length) {
-        if (arr1 == null || arr2 == null) {
-            throw new NullPointerException("Source or destination array couldn't be empty");
+    private static void arraycopy(int[] source, int sourcePos, int[] destination, int destPos, int length) {
+        if (source == null || destination == null) {
+            throw new NullPointerException("Source or destination array couldn't be null");
         }
-        if (sourcePos > arr1.length || destPos > arr2.length) {
+        if (sourcePos > source.length || destPos > destination.length) {
             throw new IndexOutOfBoundsException("Array length shorter than specified index");
         }
         for (int i = 0; i < length; i++) {
-            arr2[destPos + i] = arr1[sourcePos + i];
+            destination[destPos + i] = source[sourcePos + i];
         }
     }
 }
