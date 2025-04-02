bcc2a42e959827198335976daa643ade70918f30
Serg <supermirko9@gmail.com>
2025-04-02 23:36:50 +0300

    several improvements in GanJ

--- src/ganj/GuessANumberJava.java
+++ src/ganj/GuessANumberJava.java
@@ -6,23 +6,29 @@ import java.util.Scanner;
 public class GuessANumberJava {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        mainMenu();
-        String input = scanner.nextLine();
-        switch (input) {
-            case "1":
-                againstAI(scanner);
-                break;
-            case "2":
-                System.out.println("Soon...");
-                break;
-            case "3":
-                help();
-                break;
-            default:
-                System.out.println("Wrong input");
-                break;
+        boolean closeApp = false;
+        while (!closeApp) {
+            mainMenu();
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1":
+                    againstAI(scanner);
+                    break;
+                case "2":
+                    System.out.println("Soon...");
+                    break;
+                case "3":
+                    help();
+                    break;
+                case "4":
+                    System.out.println("Bye");
+                    closeApp = true;
+                    break;
+                default:
+                    System.out.println("Wrong input");
+                    break;
+            }
         }
-
 
     }
 
@@ -39,13 +45,15 @@ public class GuessANumberJava {
         Random random = new Random();
         System.out.println("Enter the range of numbers you want the target number to be between:");
         int bound = scanner.nextInt();
+        scanner.nextLine();
         int secretNumber = random.nextInt(bound) + 1;
         int myNumber;
         while (true) {
             System.out.println("Please input your guess number:");
             myNumber = scanner.nextInt();
+            scanner.nextLine();
             if (myNumber == secretNumber) {
-                System.out.println("Congrats, U nailed it!");
+                System.out.println("Congrats, U nailed it!\n");
                 break;
             } else if (myNumber > secretNumber) {
                 System.out.println("Your number must be less than hidden number");
@@ -61,6 +69,7 @@ public class GuessANumberJava {
                 Enter your guess
                 See if your guess is too high or too low
                 Change your guess
-                The app will tell you when you are right, and how many guesses you had""");
+                The app will tell you when you are right, and how many guesses you had
+                 """);
     }
 }
