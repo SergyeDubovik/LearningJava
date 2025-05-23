017d5b278a5b136f578d84d0af6d932c877ff632
Serg <supermirko9@gmail.com>
2025-05-23 23:53:28 +0300

    added Line intersection exercise

---
+++ src/com/epamTasks/lineIntersection/Line.java
@@ -0,0 +1,25 @@
+package com.epamTasks.lineIntersection;
+
+public class Line {
+    final int k;
+    final int b;
+
+    public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
+    }
+
+    public Point intersection(Line other) {
+        if (this.k == other.k) {
+            if (other.b == this.b) {
+                return null;
+            } else {
+                return null;
+            }
+        }
+        int x = (other.b - this.b) / (this.k - other.k);
+        int y = this.k * x + this.b;
+        return new Point(x, y);
+    }
+
+}
