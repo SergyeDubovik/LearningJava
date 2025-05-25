c6ac2bb2e1b5028c629aedda84803666ce745874
Serg <supermirko9@gmail.com>
2025-05-25 10:19:34 +0300

    added GraduallyDecreasingCarousel exercise

--- src/com/epamTasks/classes/HalvingCarousel.java
+++ src/com/epamTasks/classes/HalvingCarousel.java
@@ -1,5 +1,11 @@
 package com.epamTasks.classes;
 
+/*
+    In this exercise you need to extend DecrementingCarousel. You need to implement HalvingCarousel.
+    This subclass must halve elements instead of decrementing it by one.
+    Note that you need to apply regular integer division, discarding the remainder. For example, 5 / 2 = 2.
+ */
+
 public class HalvingCarousel  extends DecrementingCarousel {
     public HalvingCarousel(final int capacity) {
         super(capacity);
