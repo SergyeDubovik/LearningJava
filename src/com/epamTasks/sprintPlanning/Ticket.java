bcb5a21eba88dd4b0cf604656d554adadda0518a
Serg <supermirko9@gmail.com>
2025-05-26 22:47:35 +0300

    added SprintPlanning exercise

---
+++ src/com/epamTasks/sprintPlanning/Ticket.java
@@ -0,0 +1,34 @@
+package com.epamTasks.sprintPlanning;
+
+public class Ticket {
+    private int id;
+    private String name;
+    private int estimate;
+    private boolean completed = false;
+
+    public Ticket(int id, String name, int estimate) {
+        this.id = id;
+        this.name = name;
+        this.estimate = estimate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        completed = true;
+    }
+
+    public int getEstimate() {
+        return estimate;
+    }
+}
