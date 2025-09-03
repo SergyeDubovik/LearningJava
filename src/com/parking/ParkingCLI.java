9465c65422ca50c9286da87c4ef2ceb453d0ea37
serg <supermirko9@gmail.com>
2025-09-03 23:48:36 +0300

    added display and basic commands to parking's CLI

---
+++ src/com/parking/ParkingCLI.java
@@ -0,0 +1,47 @@
+package com.parking;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ParkingCLI {
+    public static void main(String[] args) {
+        Parking someParking = new ParkingImpl(10, new SimplePricingCalculator());
+        Scanner scanner = new Scanner(System.in);
+
+        display(someParking, scanner);
+
+    }
+    private static void display(Parking parking, Scanner sc) {
+        while (true) {
+            System.out.println("Welcome to our car parking");
+            System.out.println("1 - Enter");
+            System.out.println("2 - Exit");
+            System.out.println("0 - Close app");
+            String choice = sc.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println("Waiting for input car number...");
+                    String carNumber = sc.nextLine();
+                    boolean successInput = parking.enter(carNumber);
+                    if (successInput) {
+                        System.out.println("Car may parking");
+                    } else {
+                        System.out.println("Sorry, parking already full");
+                    }
+                    break;
+                case "2":
+                    System.out.println("Waiting for input car number...");
+                    String carNumberOnExit = sc.nextLine();
+                    BigDecimal pay = parking.exit(carNumberOnExit);
+                    System.out.println("Was paid: " + pay);
+                    break;
+                case "0":
+                    System.out.println("Bye");
+                    return;
+                default:
+                    System.out.println("Wrong input, try again");
+            }
+        }
+
+    }
+}
