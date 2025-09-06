289650c93b77af9534aa9a237e07e69f806e4b23
serg <supermirko9@gmail.com>
2025-09-06 19:32:19 +0300

    added opportunity to save data to the file

--- src/com/parking/ParkingImpl.java
+++ src/com/parking/ParkingImpl.java
@@ -1,17 +1,22 @@
 package com.parking;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingImpl implements Parking {
-    private int size;
-    private boolean[] isFree;
-    private PricingCalculator calculator;
-    private Map<String, ParkingRecord> visitors = new HashMap<>();
+    private final int size;
+    private final boolean[] isFree;
+    private final PricingCalculator calculator;
+    private final Map<String, ParkingRecord> visitors = new HashMap<>();
 
     public ParkingImpl(int size, PricingCalculator calculator) {
         this.size = size;
@@ -28,11 +33,27 @@ public class ParkingImpl implements Parking {
                 LocalDateTime enterCar = LocalDateTime.now().minusMinutes(110);
                 ParkingRecord record = new ParkingRecord(i, enterCar);
                 visitors.put(carNumber, record);
+                try {
+                    saveData(carNumber, enterCar, record.getSlot());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 return true;
             }
         }
         return false;
     }
+
+    private static void saveData(String carNumber, LocalDateTime enterTime, int slot) throws IOException {
+        try
+                (FileWriter fileWriter = new FileWriter("report_parking.csv", true)) {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            String formattedDate = enterTime.format(dateTimeFormatter);
+            String line = "Enter car, " + carNumber + ", " + formattedDate + ", " + "slot - " + slot + "\n";
+            fileWriter.write(line);
+        }
+    }
+
 
     @Override
     public BigDecimal exit(String carNumber) {
