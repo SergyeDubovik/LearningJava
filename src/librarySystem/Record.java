485280af909ac7711ed5822a0bfa7c16943c62dd
serg <supermirko9@gmail.com>
2025-11-26 23:58:49 +0200

    change Record class to record

--- src/librarySystem/Record.java
+++ src/librarySystem/Record.java
@@ -2,26 +2,6 @@ package librarySystem;
 
 import java.time.LocalDate;
 
-public class Record {
-    private final Customer customer;
-    private final LocalDate borrowDate;
+public record Record(Customer customer, LocalDate borrowDate) {
 
-    public Record(Customer customer, LocalDate date) {
-        this.customer = customer;
-        this.borrowDate = date;
-    }
-
-    @Override
-    public String toString() {
-        return "customer: " + customer +
-                ", date: " + borrowDate;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public LocalDate getBorrowDate() {
-        return borrowDate;
-    }
 }
