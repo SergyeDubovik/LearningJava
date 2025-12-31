5425fbbaa65201699a5cc881d3b039a032084307
serg <supermirko9@gmail.com>
2025-12-31 23:11:23 +0200

    refactored CreateFile class (added comments)

--- src/com/inputOutput/CreateFile.java
+++ src/com/inputOutput/CreateFile.java
@@ -7,27 +7,31 @@ import static testArrays.TaskArrays.bubbleSort;
 import static testArrays.TaskArrays.printArray;
 
 public class CreateFile {
+
     public static void main(String[] args) throws IOException {
         String text = "pizza better than big mac";
+
+        // Create a directory
         File folder = new File("new folder");
         folder.mkdir();
+
+        // Create a file inside the directory
         File file = new File(folder, "hello.txt");
 
         writeToFile(text, file);
         readFromFile(file);
         sortInputArray();
-
     }
-//        написать программу, которая умеет записывать в файл, то что я ввел с клавиатуры
 
+    // Write text to a file
+    // (task: write a program that can save keyboard input to a file)
     private static void writeToFile(String text, File f) throws IOException {
         try (OutputStream os = new FileOutputStream(f)) {
             os.write(text.getBytes());
         }
     }
 
-//        написать программу, которая умеет читать с файла и печатать на экран
-
+    // Read text from a file and print it to the console
     private static void readFromFile(File f) throws IOException {
         try (InputStream inStr = new FileInputStream(f)) {
             LineNumberReader nr = new LineNumberReader(new InputStreamReader(inStr));
@@ -36,18 +40,22 @@ public class CreateFile {
         }
     }
 
-//    написать программу, котоая читает массив чисел с клавы, сортирует их и печатает
-
+    // Read an array of numbers from the keyboard,
+    // sort it, and print the result
     private static void sortInputArray() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("input array size: ");
+
+        System.out.println("Input array size:");
         int size = sc.nextInt();
-        System.out.println("input " + size + " numbers below:");
+
+        System.out.println("Input " + size + " numbers:");
         int[] numbers = new int[size];
+
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
         }
-        System.out.println("Sorted array: ");
+
+        System.out.println("Sorted array:");
         bubbleSort(numbers);
         printArray(numbers);
     }
